568ad0c58cbc1a6cfb55a7123e3ecbc0fdf02f40
Ian Daniel <idaniel@spinnys.com>
2017-11-07 13:07:15 +1000

    Convert functions to lambda's

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require('express');
-var webSocketServer = require('ws').Server, 
-    wss = new webSocketServer({port: 40510});
+var app = express();
+var webSocketServer = require('ws').Server, wss = new webSocketServer({port: 40510});
 
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        console.log('received %s', message);
-    });
-
+wss.on('connection', ws => {
+    ws.on('message', message => console.log('received %s', message));
     setInterval( () => ws.send(`${new Date()}`), 1000 );
 });
 
-var app = express();
-
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '/ws.html');
-});
+app.get('/', (req,res) => res.sendFile(__dirname + '/ws.html'));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
 
